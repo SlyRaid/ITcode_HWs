a82375df4f70077d5820fdbc70f84e295e16c3de
Slyr <128023276+SlyRaid@users.noreply.github.com>
2024-04-18 12:37:12 +0500

    Update ex2-1.py

--- hw2/ex2-1.py
+++ hw2/ex2-1.py
@@ -3,16 +3,5 @@ user_input = input("Введите строку: ")
 
 
 # 1) Попросите пользователя ввести строку и выведите ее в обратном порядке, не используя метод reverse() и sorted().
-def reverse_str(user_input):
-    # Создаём пустую строку для хранения символов в обратном порядке
-    reversed_string = ""
 
-    # Перебираем символы исходной строки в обратном порядке
-    for i in range(len(user_input) - 1, -1, -1):
-        reversed_string += user_input[i]
-
-    # Выводим полученную строку в обратном порядке
-    print("Обратная строка:", reversed_string)
-
-
-reverse_str(user_input)
+print("Обратная строка:", user_input[::-1])
